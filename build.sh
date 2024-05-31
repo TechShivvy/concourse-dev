229b266badb94aaae4e4c4725c11e0a39fad0310
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2024-05-31 21:04:28 +0530

    build status

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 apk add -q --no-progress aws-cli docker
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
-build
+build_status=$(build)  # Run build command and capture its return status
+echo "$build_status" > build_status.txt  # Write the return status to a file
 
 # docker tag image "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG"
 # docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG"
