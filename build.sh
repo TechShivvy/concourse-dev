81da7d16c2a8119f923f875791c5b47a5863063e
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2024-05-31 22:45:01 +0530

    metadata

--- build.sh
+++ build.sh
@@ -4,7 +4,17 @@ apk add -q --no-progress aws-cli docker
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
 build  # Run build command
 build_status=$?  # Capture the exit status of the build command
-echo "$build_status" > build_status/build_status.txt  # Write the exit status to a file
+
+if [ $build_status -eq 0 ]; then
+    echo "Success" > build_status/build_status.txt
+else
+    echo "Failure" > build_status/build_status.txt
+fi
+
+# Append metadata to the file
+echo "Job Name: $BUILD_JOB_NAME" >> build_status/build_status.txt
+echo "Pipeline Name: $BUILD_PIPELINE_NAME" >> build_status/build_status.txt
+echo "Created By: $BUILD_CREATED_BY" >> build_status/build_status.txt
 
 # docker tag image "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG"
 # docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG"
