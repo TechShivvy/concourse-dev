e97d80677939a1c77bb884dbc4adb925dca303b7
Shivcharan Thirunavukkarasu <132028666+TechShivvy@users.noreply.github.com>
2024-05-31 20:03:10 +0530

    blah

--- app-2.py
+++ app-2.py
@@ -2,11 +2,11 @@ import platform
 
 
 def lambda_handler(event, context):
-    print("app1.py")
+    print("app2.py")
     arch = platform.machine()
     print(f"System is running on {arch}")
     return {"statusCode": 200, "body": f"This system is running on {arch}"}
 
 
-if __name__ == "__main__":
-    lambda_handler({}, {})
+# if __name__ == "__main__":
+#     lambda_handler({}, {})
